e09f3155694e360c035920c421e3f537007c8d05
KonstInt <58230320+KonstInt@users.noreply.github.com>
2024-10-09 00:06:11 +0300

    add task1

---
+++ Lab3/Lab3/FillandHighlight.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace Lab3Rastr
+{
+    partial class FillandHighlight
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(24, 26);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(899, 1045);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(940, 973);
+            this.button2.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(334, 102);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Отчистить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(938, 233);
+            this.button4.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(334, 102);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Выбрать изображение для выделения границы";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(940, 26);
+            this.radioButton1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(222, 36);
+            this.radioButton1.TabIndex = 8;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Заливка цветом";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(940, 367);
+            this.radioButton3.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(298, 36);
+            this.radioButton3.TabIndex = 10;
+            this.radioButton3.Text = "Обводка изображения";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // FillandHighlight
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1302, 1101);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.Name = "FillandHighlight";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Рисование и заливка";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.FillandHighlight_FormClosed);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button4;
+        private RadioButton radioButton1;
+        private RadioButton radioButton3;
+    }
+}
+
---
+++ Lab3/Lab3/FillandHighlight.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
+
+namespace Lab3Rastr
+{
+    public partial class FillandHighlight : Form
+    {
+        public FillandHighlight()
+        {
+            InitializeComponent();
+            g.Clear(Color.White);
+        }
+
+        // Правильное закрытие окна
+        private void FillandHighlight_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Form ifrm = Application.OpenForms[0];
+            ifrm.Show();
+        }
+
+        // Выбор цвета
+        private void button1_Click(object sender, EventArgs e)
+        {
+            colorDialog1.ShowDialog();
+        }
+
+        static Bitmap bmp = new Bitmap(485, 491); // Основное изображение
+
+        bool mouse_Down = false;
+        bool is_fill = false;
+        public Graphics g = Graphics.FromImage(bmp);
+        Pen myPen = new Pen(Color.Black, 3f); // Карандаш для рисования
+        public string img;  // Имя файла изображения для заливки
+        public Bitmap bmp_pic; // Изображение для заливки
+        public Bitmap picBmp; // Буфер изображения
+        public static Color backColor; // Цвет фона
+
+        // Координаты для рисования
+        Point[] points = new Point[2];
+
+        // Обработка нажатия мыши для начала рисования или заливки
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            mouse_Down = true;
+            is_fill = true;
+            points[0] = new Point(e.X, e.Y);
+            myPen.StartCap = System.Drawing.Drawing2D.LineCap.Round;
+            myPen.EndCap = System.Drawing.Drawing2D.LineCap.Round;
+            myPen.Color = colorDialog1.Color;
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            mouse_Down = false;
+
+            if (radioButton1.Checked) // Если выбрана заливка цветом
+            {
+                if (is_fill)
+                {
+                    FloodFill(e.X, e.Y);
+                    pictureBox1.Image = bmp;
+                }
+            }
+            else if (radioButton3.Checked) // Если выбрана заливка изображением
+            {
+                if (is_fill)
+                {
+                    FloodFillWithImage(e.X, e.Y);
+                    pictureBox1.Image = bmp;
+                }
+            }
+            else if (radioButton3.Checked) // Если выбрано выделение границы
+            {
+                if (is_fill)
+                    Connected(e.X, e.Y);
+            }
+        }
+
+        // Рисование линии при движении мыши
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            is_fill = false;
+            if (mouse_Down)
+            {
+                points[1] = new Point(e.X, e.Y);
+                g.DrawLines(myPen, points);
+                pictureBox1.Image = bmp;
+                points[0] = points[1];
+                points[1] = new Point(e.X, e.Y);
+            }
+        }
+
+        // Рекурсивная заливка цветом
+        public void FloodFill(int x, int y)
+        {
+            Color targetColor = bmp.GetPixel(x, y); // Исходный цвет
+            Color fillColor = colorDialog1.Color; // Цвет заливки
+
+            if (targetColor == fillColor) return; // Прекращаем, если цвета совпадают
+
+            FloodFillRec(x, y, targetColor, fillColor);
+        }
+
+        // Рекурсивная функция заливки
+        private void FloodFillRec(int x, int y, Color targetColor, Color fillColor)
+        {
+            if (x < 0 || x >= bmp.Width || y < 0 || y >= bmp.Height) return;
+            if (bmp.GetPixel(x, y) != targetColor) return;
+
+            bmp.SetPixel(x, y, fillColor);
+
+            FloodFillRec(x + 1, y, targetColor, fillColor);
+            FloodFillRec(x - 1, y, targetColor, fillColor);
+            FloodFillRec(x, y + 1, targetColor, fillColor);
+            FloodFillRec(x, y - 1, targetColor, fillColor);
+        }
+
+        // Рекурсивная заливка изображением (паттерном)
+        public void FloodFillWithImage(int x, int y)
+        {
+            if (bmp_pic == null) return; // Если нет изображения для заливки
+
+            Color targetColor = bmp.GetPixel(x, y);
+            FloodFillImageRec(x, y, targetColor);
+        }
+
+        private void FloodFillImageRec(int x, int y, Color targetColor)
+        {
+            if (x < 0 || x >= bmp.Width || y < 0 || y >= bmp.Height) return;
+            if (bmp.GetPixel(x, y) != targetColor) return;
+
+            int imgX = x % bmp_pic.Width;
+            int imgY = y % bmp_pic.Height;
+            bmp.SetPixel(x, y, bmp_pic.GetPixel(imgX, imgY));
+
+            FloodFillImageRec(x + 1, y, targetColor);
+            FloodFillImageRec(x - 1, y, targetColor);
+            FloodFillImageRec(x, y + 1, targetColor);
+            FloodFillImageRec(x, y - 1, targetColor);
+        }
+
+        // Загрузка изображения
+        private void button4_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                ofd.Filter = "Image Files|*.jpeg;*.jpg;*.png";
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    this.img = ofd.FileName;
+                    bmp_pic = new Bitmap(this.img);
+                }
+                pictureBox1.Image = bmp_pic;
+            }
+        }
+
+        // Очистка изображения
+        private void button2_Click(object sender, EventArgs e)
+        {
+            g.Clear(Color.White);
+            pictureBox1.Image = bmp;
+        }
+
+        // Алгоритм выделения границы области
+        void Connected(int x, int y)
+        {
+            List<Point> boundary = new List<Point>();
+            Color targetColor = bmp_pic.GetPixel(x, y);
+            TraceBoundary(x, y, targetColor, boundary);
+            DrawBoundary(boundary);
+        }
+
+        // Обход границы
+        private void TraceBoundary(int x, int y, Color targetColor, List<Point> boundary)
+        {
+            if (x < 0 || x >= bmp_pic.Width || y < 0 || y >= bmp_pic.Height) return;
+            if (bmp_pic.GetPixel(x, y) != targetColor) return;
+
+            Stack<Point> stack = new Stack<Point>();
+            stack.Push(new Point(x, y));
+
+            while (stack.Count > 0)
+            {
+                Point p = stack.Pop();
+                int px = p.X;
+                int py = p.Y;
+
+                if (bmp_pic.GetPixel(px, py) == targetColor)
+                {
+                    boundary.Add(p);
+                    bmp_pic.SetPixel(px, py, Color.Black); // Прорисовка границы
+
+                    if (px + 1 < bmp_pic.Width) stack.Push(new Point(px + 1, py));
+                    if (px - 1 >= 0) stack.Push(new Point(px - 1, py));
+                    if (py + 1 < bmp_pic.Height) stack.Push(new Point(px, py + 1));
+                    if (py - 1 >= 0) stack.Push(new Point(px, py - 1));
+                }
+            }
+        }
+
+        // Отрисовка границы
+        private void DrawBoundary(List<Point> boundary)
+        {
+            foreach (Point p in boundary)
+            {
+                bmp.SetPixel(p.X, p.Y, Color.Blue); // Рисуем синий цвет для границы
+            }
+            pictureBox1.Image = bmp;
+        }
+
+       
+    }
+}
